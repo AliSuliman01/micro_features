c603e98aaab2870f48b6ec25d5603edb28c8a875
Ali Suliman <alisulimanq@gmail.com>
2022-09-18 17:18:02 +0300

    - fix Log middleware: read response using getContent

--- src/Http/Middleware/Log.php
+++ src/Http/Middleware/Log.php
@@ -7,7 +7,9 @@ use AliSuliman\MicroFeatures\Exceptions\AuthorizationException;
 use AliSuliman\MicroFeatures\Exceptions\Exception;
 use AliSuliman\MicroFeatures\Facades\Auth;
 use AliSuliman\MicroFeatures\Helpers\StatusCode;
+use AliSuliman\MicroFeatures\Helpers\UserAgent;
 use AliSuliman\MicroFeatures\Jobs\BroadcastJob;
+use AliSuliman\MicroFeatures\Jobs\ExecJob;
 use AliSuliman\MicroFeatures\Jobs\StoreJob;
 use Firebase\JWT\JWT;
 use Firebase\JWT\Key;
@@ -27,13 +29,15 @@ class Log
     {
         $jsonRequest = $request->json()->all();
         $response = $next($request);
-        $jsonResponse = $response->getJson();
+        $jsonResponse = $response->getContent();
 
-        dispatch(new BroadcastJob(new StoreJob('activity_logs',[
-            'jsonRequest' => $jsonRequest,
-            'jsonResponse' => $jsonResponse,
-            'activity_type' => $request->getUri()
-        ]),['logs']));
+        dispatch(new BroadcastJob(new ExecJob('activity_logs','store',
+            UserAgent::createUserActivityRequest([
+                'jsonRequest' => $jsonRequest,
+                'jsonResponse' => $jsonResponse,
+            ]),
+            Auth::$rpcToken
+        ),['logs']));
 
         return $response;
     }
